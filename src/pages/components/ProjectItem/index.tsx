b8a9d36426babb1ea67e7091ac17cb54ab230568
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2023-05-15 19:18:01 -0300

    Deploy

--- src/pages/components/ProjectItem/index.tsx
+++ src/pages/components/ProjectItem/index.tsx
@@ -1,7 +1,7 @@
-import Link from 'next/link';
 import { GrDeploy } from 'react-icons/gr';
 import { AiFillGithub } from 'react-icons/ai';
 
+import Link from 'next/link';
 import { Container } from './styles';
 
 interface ProjectItemProps {
